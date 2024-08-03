fde6623b6ab02e250e2c13a5d17cf0ddcb0ce3c9
Yosef elbooz <yoosefelbooz@gmail.com.com>
2024-08-03 17:26:00 +0300

    Your branch is up to date with 'origin/main'.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,48 +9,7 @@ import emailjs from '@emailjs/browser';
 
 import EarthCanvas from './canvas/Earth';
 const Contact = () => {
-  // const formRef=useRef();
-  // const [form,setForm]=useState({
-  //   name:' ',
-  //   email:' ',
-  //   message:' ',
-  // });
-  // const [load, setload] = useState(false);
-  // const handelChange=(e)=>{
-  //   const {name,value}=e.target;
-  //   setForm({...form,[name]:value});
-  // }
-  // const handelSumbit=(e)=>{
-  //   e.preventDefault();
-  //   setload(true);
-  //   emailjs.send(
-  //       "service_n6zbha4",
-  //       "template_ahol33o",
-  //     {
-  //       from_name: form.name,
-  //       to_name: "Yosef",
-  //       from_email: form.email,
-  //       to_email: "yoosefelbooz@gmail.com",
-  //       message: form.message,
-  //     }, 
-  //        "Es3UPA2i09dR9pwKL"
-  //   ).then(() => {
-  //       setLoading(false);
-  //       alert('Thank you. I will get back to you as soon as possible.');
 
-  //       setForm({
-  //         name: "",
-  //         email: "",
-  //         message: "",
-          
-  //       });
-  //   }), (error) => {
-  //     setLoading(false);
-  //     console.error(error);
-
-  //     alert("Ahh, something went wrong. Please try again.");
-  //   }
-  // }
   const formRef = useRef();
   const [form, setForm] = useState({
     name: "",
@@ -107,18 +66,17 @@ const Contact = () => {
       );
   };
   return (
-    <div className='xl:mt-12 xl:flex-row flex-col-reverse
-     flex gap-10 overflow-hidden'>
+    <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
         <motion.div
         variants={slideIn('left','tween',0.2,1)}
-        className=' flexr rounded-2xl bg-black-100 p-8'
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
         >
           <p className={styles.sectionSubText}> Get in touch</p>
           <h3 className={styles.sectionHeadText}>Contact.</h3>
           <form 
           ref={formRef}
           onSubmit={handleSubmit}
-          className=' mt-12 flex-col gap-8'
+          className='mt-12 flex flex-col gap-8'
           >
             <label className='flex flex-col'>
               <span className=' text-white font-medium mb-4'>Your Name</span>
@@ -134,7 +92,8 @@ const Contact = () => {
             </label>
             <label className='flex flex-col'>
               <span className=' text-white font-medium mb-4'>Your Email</span>
-              <input type="email" 
+              <input 
+              type="email" 
               name='email'
               value={form.email}
               placeholder="What's your email?"
@@ -147,6 +106,7 @@ const Contact = () => {
               <span className=' text-white font-medium mb-4'>Your Message</span>
               <textarea 
               rows="7"
+              type='text'
               name='message'
               value={form.message}
               onChange={handleChange}
@@ -156,9 +116,7 @@ const Contact = () => {
                rounded-lg outlined-none border-none font-medium'/>
             </label>
               <button type='submit'
-              className=' bg-tertiary py-3 px-8
-              text-white mt-3
-              outline-none w-fitt font-bold shadow-md shadow-primary rounded-xl'
+              className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
               onClick={handleSubmit}
               >
               {load?'Sending...':'send'}
